411cfaf05ff8033ba4f15a4accd42b0c0a8fdd39
huzhiyong-ui <464103835@qq.com>
2019-07-25 17:00:52 +0800

    提交代码

---
+++ Project3/Project3/源.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		int j = 0;
+		for (j = 2; j <=i; j++)
+		{
+			if (i%j == 0)
+				break;
+			if (i == j)
+				printf("%d ", i);
+		}
+	}
+	system("pause");
+	return 0;
+}
